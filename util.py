a0885f3de97b1f37928fed45026b04d8dfd1e059
AlexArguetaNet <alexarguetanet@gmail.com>
2025-09-18 23:52:38 -0400

    Implemented create_files() function in util.py

--- util.py
+++ util.py
@@ -2,17 +2,35 @@ import os
 import sys
 
 def create_files():
-
-    dir_name = input("Enter directory to save files: ")
-    num_of_files = int(input("How many files?: "))
-
-    for i in range(num_of_files):
-        file_name = input("Enter file name: ")
+    while True:
         try:
-            with open(f"{dir_name}/{file_name}", "a"):
-                pass
-        except PermissionError:
-            print("Permission denied")
+            dir_name = input("\nEnter directory to save files\n(Press enter to use current directory): ")
+            if dir_name == "":
+                dir_name = os.getcwd()
+
+            if not os.path.exists(dir_name) or not os.access(dir_name, os.W_OK):
+                print("\n*****************")
+                print("Directory invalid")
+                print("*****************")
+                continue
+            else:
+                num_of_files = int(input("How many files?: "))
+
+                while num_of_files != 0:
+                    file_name = input("Enter file name: ")
+                    try:
+                        with open(f"{dir_name}/{file_name}", "a"):
+                            pass
+                        num_of_files -= 1
+                    except FileExistsError:
+                        print("\n*********************")
+                        print("File already exists")
+                        print("*********************\n")
+                        continue
+
+                return
+        except KeyboardInterrupt:
+            sys.exit()
 
 def create_folders():
     while True: # Run loop until valid directoy name is given
