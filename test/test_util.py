fbf660dd6224824a45da87dd49441bb6732f1235
AlexArguetaNet <alexarguetanet@gmail.com>
2025-10-02 00:45:49 -0400

    Added more test functions in test_util.py

--- test/test_util.py
+++ test/test_util.py
@@ -1,6 +1,6 @@
 
 import os
-from util import create_files, create_folders
+from util import create_files, create_folders, validate_path, path_exists
 
 def test_create_files():
     paths = [f"{os.getcwd()}/f1", f"{os.getcwd()}/f2", f"{os.getcwd()}/-3123aFmei", f"{os.getcwd()}/-.."]
@@ -13,3 +13,24 @@ def test_create_folders():
     assert create_folders(paths) == True
     for path in paths:
         os.removedirs(path)
+
+def test_validate_path_is_valid():
+    path = f"{os.getcwd()}/test-path"
+    os.mkdir(path)
+    assert validate_path(path) == path
+    os.rmdir(path)
+
+def test_validate_path_not_valid():
+    assert validate_path(f"{os.getcwd()}/fake-path") == None
+
+def test_path_exists_true():
+    path = f"{os.getcwd()}/test-folder"
+    os.mkdir(path)
+    assert path_exists(path, [], "folders") == True
+    assert path_exists(path, ["home/user/Documents", f"{os.getcwd()}/test-folder"], "folders") == True
+    os.rmdir(path)
+
+def test_path_exists_false():
+    path = f"{os.getcwd()}/test-folder"
+    assert path_exists(path, [], "folders") == False
+    assert path_exists(path, ["home/user/Documents"], "folders") == False
