d51743516353775b3154917d9976a57a268f072e
AlexArguetaNet <alexarguetanet@gmail.com>
2025-10-09 15:59:54 -0400

    Added 'back' option for file and folder menu

--- main.py
+++ main.py
@@ -20,25 +20,31 @@ def main():
             continue
 
 def files_menu():
-    option = create_menu("Files", ["Create new files", "Write to file"])
+    option = create_menu("Files", ["Create new files", "Write to file", "Back"])
     if option == 0:
         print("Enter path to create files: ", end="")
         path_name = get_path()
         file_paths = get_new_paths(path_name, "file")
         create_files(file_paths)
     elif option == 1:
-        # TODO: Check if path is a directory or a file
         file_path = input("Enter file path: ")
         write_to_file(file_path)
+    else:
+        pass
 
 
 def folders_menu():
-    option = create_menu("Folders", ["Create new folders", "Add files to folder"])
+    option = create_menu("Folders", ["Create new folders", "Add files to folder", "Back"])
     if option == 0:
         print("Enter path to create folders: ", end="")
         path_name = get_path()
         folder_paths = get_new_paths(path_name, "folder")
         create_folders(folder_paths)
+    elif option == 1:
+        # TODO: Implement function to add files to a new folder
+        pass
+    else:
+        pass
 
 def get_path():
     while True:
